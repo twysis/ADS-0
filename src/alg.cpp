22cca8286fc47ed60815ed7eaa3898768e8c995c
Матвей Головенко <91070065+twysis@users.noreply.github.com>
2022-02-12 13:08:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,11 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    int ost;
+    while (a % b != 0) {
+        ost = a % b;
+        a = b;
+        b = ost;
+    }
+    return b;
 }
